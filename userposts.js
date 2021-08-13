d34c5e2589b5d36e80bdc90b35bfc839478e3f7f
SrilakshmiLagadapati <lagadapati.srilakshmi95@gmail.com>
2021-08-13 16:44:51 +0530

    fetched user details using link and validated for the login

--- userposts.js
+++ userposts.js
@@ -4,7 +4,11 @@ let nameEl = document.getElementById("name");
 let passwordErrMsgEl = document.getElementById("passwordErrMsg");
 let passwordEl = document.getElementById("password");
 let buttonEl = document.getElementById("button");
-let navHeadingEl = document.getElementById("nav-heading")
+let navContainerEl = document.getElementById("nav-container")
+let nameEntered = "";
+let passwordEntered = "";
+let jsonDeatils = "";
+navContainerEl.classList.add("d-none")
 nameEl.addEventListener("change", function(event) {
     if (event.target.value === "") {
         nameErrMsgEl.textContent = "Required*";
@@ -25,3 +29,38 @@ myFormEl.addEventListener("submit", function(event) {
     event.preventDefault();
 
 });
+function getUserDetails() {
+    let option = {
+        method: "GET"
+    };
+
+    fetch("https://60fe481525741100170784ff.mockapi.io/api/v1/user", option)
+        .then(function(response) {
+            return response.json();
+
+        })
+        .then(function(jsonData) {
+            jsonDeatils = jsonData;
+            nameEntered = nameEl.value;
+        });
+}
+function displayUserDetails(nameEntered, passwordEntered) {
+
+    for (let item of jsonDeatils) {
+        if ((item.password === passwordEntered) && (item.name === nameEntered)) {
+            detailsMatched(nameEntered);
+            navContainerEl.classList.remove("d-none")
+            myFormEl.classList.add("d-none");
+            document.getElementById("heading").classList.add("d-none");
+
+        } else {
+            document.getElementById("totalErrMsg").textContent = "Invalid user details";
+        }
+    }
+}
+buttonEl.onclick = function() {
+    let nameEntered = nameEl.value;
+    let passwordEntered = passwordEl.value;
+    getUserDetails()
+    displayUserDetails(nameEntered, passwordEntered);
+};
