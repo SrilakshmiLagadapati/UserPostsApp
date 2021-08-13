1e52540328f130fddc80607872d539c009750a60
SrilakshmiLagadapati <lagadapati.srilakshmi95@gmail.com>
2021-08-13 17:40:53 +0530

    fetched posts details and added navbar using bootswatch

--- userposts.js
+++ userposts.js
@@ -44,6 +44,74 @@ function getUserDetails() {
             nameEntered = nameEl.value;
         });
 }
+function displayPostDetails(nameEntered) {
+
+    let navHeadingEl = document.getElementById("nav-heading")
+    navHeadingEl.textContent = nameEntered
+    let logOutEl = document.getElementById("log-out")
+    let userDetails = document.getElementById("sectionUser");
+    
+
+    for (let item of jsonDeatils) {
+        if (item.name === nameEntered) {
+           let logoAvatar = document.getElementById("logo-avatar")
+           logoAvatar.src = item.avatar
+        }
+    }
+    logOutEl.onclick = function(){
+        
+        myFormEl.classList.remove("d-none");
+        document.getElementById("heading").classList.remove("d-none");
+        userDetails.textContent = "";
+        totalErrMsg.textContent = "";
+        nameEl.value= "";
+        passwordEl.value = "";
+    }
+
+    for (let item of postJsondeatils) {
+        userDetails.classList.add("main-container");
+        let image = item.avatar;
+
+        let divEl = document.createElement("div");
+        userDetails.appendChild(divEl);
+        divEl.classList.add("container-div");
+
+        let avatar = document.createElement("img");
+        avatar.src = image;
+        avatar.classList.add("avatar");
+        divEl.appendChild(avatar);
+
+        let headingName = document.createElement("p");
+        headingName.textContent = item.name;
+        headingName.classList.add("name");
+        divEl.appendChild(headingName);
+
+        let para = document.createElement("p");
+        para.textContent = item.post;
+        para.classList.add("post");
+        userDetails.appendChild(para);
+    }
+}
+
+function detailsMatched(nameEntered) {
+    let userDetails = document.getElementById("sectionUser");
+
+
+    let option = {
+        method: "GET"
+    };
+
+    fetch("https://60fe481525741100170784ff.mockapi.io/api/v1/user_post", option)
+        .then(function(response) {
+            return response.json();
+
+        })
+        .then(function(jsonData) {
+            postJsondeatils = jsonData;
+            displayPostDetails(nameEntered);
+        });
+
+}
 function displayUserDetails(nameEntered, passwordEntered) {
 
     for (let item of jsonDeatils) {
